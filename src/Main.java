b228592846ef5fb6b0f7c7f6604a7165f9f6cec0
Shiotsuki KAITO <e195754@ie.u-ryukyu.ac.jp>
2019-12-09 22:45:53 +0900

    エラー

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,9 @@
 public class Main {
-    public static void main(String[] args){
-        try{
-            String str = null;
-            System.out.println(str.length());
-        }catch(NullPointerException e){
-            System.out.println("エラー「NullPointerExeption」が検出された");
-            System.out.println(e.getMessage());
+    public static void main(String[] args) {
+        try {
+            String str = "百二十三";
+            int value = Integer.parseInt(str);
+            System.out.println(value);
         }
-
     }
 }
